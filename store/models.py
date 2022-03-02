58ad2144cae91a8a24810398ccd3afefb190c60a
sharath007 <nsharathkumar007@gmail.com>
2022-03-01 20:05:42 -0500

    Store and Cart functions added and working fine

--- store/models.py
+++ store/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 from django.urls import reverse
 from category.models import Category
@@ -8,6 +9,7 @@ class Product(models.Model):
     slug = models.SlugField(max_length=200)
     provider = models.CharField(max_length=200, blank=True)
     license_number = models.CharField(max_length=200, blank=True)
+    description = models.TextField(max_length=500, blank=True)
     address = models.TextField(max_length=500, blank=True)
     mandal = models.CharField(max_length=200, blank=True)
     district = models.CharField(max_length=200, blank=True)
@@ -27,3 +29,28 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+
+
+class VariationManager(models.Manager):
+    def grades(self):
+        return super(VariationManager,self).filter(variation_category='grade',is_active=True)
+
+    def sizes(self):
+        return super(VariationManager,self).filter(variation_category='size',is_active=True)
+
+
+variation_category_choice = {
+    ('grade','grade'),
+    ('size','size'),
+}
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    variation_category = models.CharField(max_length=100, choices=variation_category_choice)
+    variation_value = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+    objects = VariationManager()
+
+    def __str__(self):
+        return self.variation_value
