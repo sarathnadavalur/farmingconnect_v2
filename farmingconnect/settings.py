effe3e3c2c42e5d6e5c18638fa65507cecee8073
sharath007 <nsharathkumar007@gmail.com>
2022-03-05 19:36:05 -0500

    Password forgot and reset - completed and working

--- farmingconnect/settings.py
+++ farmingconnect/settings.py
@@ -140,3 +140,17 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+#SMTP CONFIGURATION
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'farmingconnectinc@gmail.com'
+EMAIL_HOST_PASSWORD = 'html1234'
+EMAIL_USE_TLS = True
--- farmingconnect/views.py
+++ farmingconnect/views.py
@@ -4,8 +4,10 @@ from store.models import Product
 
 
 def home(request):
+
     products = Product.objects.all().filter(is_available=True)
     context = {
         'products': products,
+        'loop_count' : range(0, 8),
     }
     return render(request,'home.html', context)
