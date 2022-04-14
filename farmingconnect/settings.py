d508bbd4f8689d1b4b0551f2d47c1ea144bb3c8e
Sarath <nsharathkumar007@gmail.com>
2022-04-14 19:51:08 -0400

    Update settings.py

--- farmingconnect/settings.py
+++ farmingconnect/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,7 +126,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = BASE_DIR /'staticfiles'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS =[
